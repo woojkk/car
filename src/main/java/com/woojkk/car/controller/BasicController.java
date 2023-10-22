d3af8f32d9172c0eed6f8eae5e29ed63ae4b89b6
woojin Kim <woojkk@naver.com>
2023-10-23 00:27:29 +0900

    Controller 추가

---
+++ src/main/java/com/woojkk/car/controller/BasicController.java
@@ -0,0 +1,46 @@
+package com.woojkk.car.controller;
+
+import com.woojkk.car.domain.Company;
+import com.woojkk.car.domain.CompanyInputDto;
+import com.woojkk.car.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BasicController {
+
+  private final CompanyService companyService;
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/companyForm")
+  public String companyForm() {
+    return "companyForm";
+  }
+
+  @PostMapping("/saveCompany")
+  public String saveCompany(@ModelAttribute(name = "companyInputDto")CompanyInputDto companyInputDto) {
+    companyService.saveCompanyInputDto(companyInputDto);
+
+    return "index";
+  }
+
+  @GetMapping("/companyList")
+  public String companyList(@PageableDefault Pageable pageable, Model model) {
+    Page<Company> companyList = companyService.getACompanyPage(pageable);
+    model.addAttribute("companyList", companyList);
+
+    return "companyList";
+  }
+}
